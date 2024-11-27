c8a76e9d83d29d8a6f4ea8518d353307dca50a10
Samory Sundjata <samory.sundjata@gmail.com>
2024-11-27 13:57:42 -0300

    Adicione novas rotas para gerenciamento de contas, incluindo recuperação, atualização e exclusão, e atualize accounts.json com novos dados

--- controllers/account.controller.js
+++ controllers/account.controller.js
@@ -27,6 +27,99 @@ async function createAccount(req, res, next) {
   }
 }
 
+async function getAccounts(req, res, next) {
+  try {
+    const data = JSON.parse(await readFile(filename));
+    delete data.nextId;
+    res.send(data);
+    logger.info("GET /account");
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function getAccount(req, res, next) {
+  try {
+    const data = JSON.parse(await readFile(filename));
+    const account = data.accounts.find(
+      (account) => account.id === parseInt(req.params.id)
+    );
+    res.send(account);
+    logger.info("GET /account/:id");
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function deleteAccount(req, res, next) {
+  try {
+    const data = JSON.parse(await readFile(filename));
+    data.accounts = data.accounts.filter(
+      (account) => account.id !== parseInt(req.params.id)
+    );
+    await writeFile(filename, JSON.stringify(data, null, 2));
+    res.end();
+    logger.info("DELETE /account/:id - ${req.params.id}");
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function updateAccount(req, res, next) {
+  try {
+    const account = req.body;
+
+    if (!account.id || !account.name || account.balance == null) {
+      throw new Error("Id, Name e Balance são obrigatórios");
+    }
+
+    const data = JSON.parse(await readFile(global.filename));
+    const index = data.accounts.findIndex((a) => a.id === account.id);
+
+    if (index === -1) {
+      throw new Error("Registro não encontrado");
+    }
+
+    data.accounts[index].name = account.name;
+    data.accounts[index].balance = account.balance;
+    await writeFile(global.filename, JSON.stringify(data, null, 2));
+
+    res.send(account);
+    logger.info(`PUT /account - ${JSON.stringify(account)}`);
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function updateBalance(req, res, next) {
+  try {
+    const account = req.body;
+    const data = JSON.parse(await readFile(global.filename));
+    const index = data.accounts.findIndex((a) => a.id === account.id);
+
+    if (!account.id || account.balance == null) {
+      throw new Error("Id e Balance são obrigatórios");
+    }
+
+    if (index === -1) {
+      throw new Error("Registro não encontrado");
+    }
+
+    data.accounts[index].balance = account.balance;
+    await writeFile(global.filename, JSON.stringify(data, null, 2));
+
+    res.send(data.accounts[index]);
+    logger.info(`PATCH /account - ${JSON.stringify(account)}`);
+  } catch (err) {
+    next(err);
+  }
+}
+
 export default {
   createAccount,
+  getAccounts,
+  getAccount,
+  deleteAccount,
+  updateAccount,
+  updateBalance,
 };
--- routes/accounts.routes.js
+++ routes/accounts.routes.js
@@ -1,101 +1,14 @@
-import { log } from "console";
 import express from "express";
-import { promises as fs } from "fs";
-import cors from "cors";
 import AccountController from "../controllers/account.controller.js";
 
-const { readFile, writeFile } = fs;
 const router = express.Router();
 
 router.post("/", AccountController.createAccount);
-
-router.get("/", cors(), async (req, res, next) => {
-  try {
-    const data = JSON.parse(await readFile(filename));
-    delete data.nextId;
-    res.send(data);
-    logger.info("GET /account");
-  } catch (err) {
-    next(err);
-  }
-});
-
-router.get("/:id", async (req, res, next) => {
-  try {
-    const data = JSON.parse(await readFile(filename));
-    const account = data.accounts.find(
-      (account) => account.id === parseInt(req.params.id)
-    );
-    res.send(account);
-    logger.info("GET /account/:id");
-  } catch (err) {
-    next(err);
-  }
-});
-
-router.delete("/:id", async (req, res, next) => {
-  try {
-    const data = JSON.parse(await readFile(filename));
-    data.accounts = data.accounts.filter(
-      (account) => account.id !== parseInt(req.params.id)
-    );
-    await writeFile(filename, JSON.stringify(data, null, 2));
-    res.end();
-    logger.info("DELETE /account/:id - ${req.params.id}");
-  } catch (err) {
-    next(err);
-  }
-});
-
-router.put("/", async (req, res, next) => {
-  try {
-    const account = req.body;
-
-    if (!account.id || !account.name || account.balance == null) {
-      throw new Error("Id, Name e Balance são obrigatórios");
-    }
-
-    const data = JSON.parse(await readFile(global.filename));
-    const index = data.accounts.findIndex((a) => a.id === account.id);
-
-    if (index === -1) {
-      throw new Error("Registro não encontrado");
-    }
-
-    data.accounts[index].name = account.name;
-    data.accounts[index].balance = account.balance;
-    await writeFile(global.filename, JSON.stringify(data, null, 2));
-
-    res.send(account);
-    logger.info(`PUT /account - ${JSON.stringify(account)}`);
-  } catch (err) {
-    next(err);
-  }
-});
-
-router.patch("/updateBalance", async (req, res, next) => {
-  try {
-    const account = req.body;
-    const data = JSON.parse(await readFile(global.filename));
-    const index = data.accounts.findIndex((a) => a.id === account.id);
-
-    if (!account.id || account.balance == null) {
-      throw new Error("Id e Balance são obrigatórios");
-    }
-
-    if (index === -1) {
-      throw new Error("Registro não encontrado");
-    }
-
-    data.accounts[index].balance = account.balance;
-    await writeFile(global.filename, JSON.stringify(data, null, 2));
-
-    res.send(data.accounts[index]);
-    logger.info(`PATCH /account - ${JSON.stringify(account)}`);
-  } catch (err) {
-    next(err);
-  }
-});
+router.get("/", AccountController.getAccounts);
+router.get("/:id", AccountController.getAccount);
+router.delete("/:id", AccountController.deleteAccount);
+router.put("/", AccountController.updateAccount);
+router.patch("/updateBalance", AccountController.updateBalance);
 
 router.use((err, req, res, next) => {
   logger.error(`${req.method} ${req.baseUrl} -  ${err.message}`);
